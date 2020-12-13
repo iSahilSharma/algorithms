c86376044a7e4ab19a8c1fa027607abd6424333e
Sahil Sharma <isahilsharma@outlook.com>
2020-12-13 21:51:57 +0530

    Code restructuring

--- Algorithms.Run/Program.cs
+++ Algorithms.Run/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Algorithms.HackerRank;
+//using Algorithms.HackerRank;
+//using Algorithms.SortingAlgorithms;
 
 namespace Algorithms.Run
 {
@@ -8,6 +9,7 @@ namespace Algorithms.Run
         static void Main(string[] args)
         {
             //SockMerchant.CountSockPair(9, new int[] { 10, 10, 1, 1, 1, 1, 1, 1, 1 });
+            //InsertionSort.Sort(new int[] {9,1,4,6,2,8});
             Console.WriteLine("Hello World!");
         }
     }
